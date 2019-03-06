f8a9d990a5949f1be7f94f2f8d112315459a311c
Dougal MacPherson <dm@trineo.co.nz>
2019-03-07 08:27:18 +1300

    Add in router and large form with 100 fields

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Login from "./login"
+import LargeForm from "./large-form"
 import "./app.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Login />
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/large-form" component={LargeForm} />
+          <Login />
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ src/large-form/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+import FormWrapper from "../form-wrapper"
+import "./large-form.css"
+
+const generateFields = size => {
+  const fields = []
+  for (let index = 1; index <= size; index++) {
+    fields.push(index)
+  }
+
+  return fields
+}
+
+const LargeForm = () => (
+  <FormWrapper>
+    <h1>Lots of fields</h1>
+    <fieldset>
+      <div className="largeForm--fields">
+        {generateFields(100).map(field => (
+          <div key={field}>
+            <label htmlFor={field}>Field Nº{field}</label>
+            <input id={field} name={field} />
+          </div>
+        ))}
+      </div>
+    </fieldset>
+
+    <fieldset>
+      <button type="submit">Save</button>
+    </fieldset>
+  </FormWrapper>
+)
+
+export default LargeForm
